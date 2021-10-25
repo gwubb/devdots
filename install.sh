21253912d10722f5cc4497e5d9434195212fa15d
gwubb <gwubbels08@icloud.com>
2021-10-25 15:46:25 -0700

    it's still bad

--- install.sh
+++ install.sh
@@ -5,8 +5,8 @@ git pull
 #Links the neovim folder and zshrc to their respective spots.
 echo "Linking Neovim and zshrc."
 git submodule update --init --depth 1
-ln -s ~/devdots/.zshrc ~/
-ln -s ~/devdots/nvim/ ~/.config/
+ln -s .zshrc ~/
+ln -s nvim/ ~/.config/
 
 #macos package install
 echo "Installing homebrew..."
@@ -20,9 +20,6 @@ if [ ! -f /usr/local/bin/brew ]; then
 else
     echo "Homebrew already installed, skipping..."
 fi
-
-brew install "$(cat homebrew_packages)"
-brew cask install "$(cat homebrew-cask_packages)"
 
 echo "done"
 
